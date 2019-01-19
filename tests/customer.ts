11b4f594e35b580562c0947b4d09e64371a7ee0b
marinakulik <maryna.kulyk2015@gmail.com>
2019-01-19 23:05:31 +0200

    first tests

--- tests/customer.ts
+++ tests/customer.ts
@@ -14,7 +14,7 @@ describe('Customer', function () {
         expect($('#box-customer-service').isVisible()).to.be.true;
     });
 
-    it.skip("Fills in the form inputs", function() {
+    it("Fills in the form inputs", function() {
         const contactForm = $('form[name="contact_form"]');
         const contactEmail = contactForm.$('input[name="email"]');
 
@@ -29,10 +29,45 @@ describe('Customer', function () {
         expect($('.alert-danger').isVisible()).to.be.true;
     });
 
-    it("Searching from element to element", function() {
-        const informationLinks = $('#box-information-links');
-        const cookiePolicy = informationLinks.$('ul.nav-pills li:first-of-type a');
+    const informationLinks = $('#box-information-links');
+
+    it("Opens cookie policy page", function() {
+        const cookiePolicy = informationLinks.$('.nav-pills li:first-of-type a');
         cookiePolicy.click();
-        browser.pause(2000);
+        browser.pause(1000);
+        const cookieTitle = browser.getTitle();
+        expect(cookieTitle).includes('Cookie Policy');
+    });
+
+    it("Opens delivery info page", function() {
+        const deliveryInfo = informationLinks.$('.nav-pills li:nth-of-type(2) a');
+        deliveryInfo.click();
+        browser.pause(1000);
+        const deliveryTitle = browser.getTitle();
+        expect(deliveryTitle).includes('Delivery Information');
+    });
+
+    it("Opens newsletter page", function() {
+        const newsletterPage = informationLinks.$('.nav-pills li:nth-of-type(3) a');
+        newsletterPage.click();
+        browser.pause(1000);
+        const newsletterTitle = browser.getTitle();
+        expect(newsletterTitle).includes('Newsletter');
+    });
+
+    it("Opens privacy page", function() {
+        const privacyPage = informationLinks.$('.nav-pills li:nth-of-type(4) a');
+        privacyPage.click();
+        browser.pause(1000);
+        const privacyPolicy = browser.getTitle();
+        expect(privacyPolicy).includes('Privacy Policy');
+    });
+
+    it("Opens terms page", function() {
+        const termsPage = informationLinks.$('.nav-pills li:nth-of-type(5) a');
+        termsPage.click();
+        browser.pause(1000);
+        const termsTitle = browser.getTitle();
+        expect(termsTitle).includes('Terms & Conditions');
     });
 });
