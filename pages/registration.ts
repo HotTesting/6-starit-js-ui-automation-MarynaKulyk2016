50b87f5affcec378b5b9a7080c18b77f17b3993f
marinakulik <maryna.kulyk2015@gmail.com>
2019-02-10 20:32:32 +0200

    authorization page objects

--- pages/registration.ts
+++ pages/registration.ts
@@ -15,37 +15,31 @@ class Registration extends BasePage{
     }
 
     public typeFirstName() {
-        browser.pause(2000);
         $(this.firstNameField).waitForVisible();
         $(this.firstNameField).setValue('First name');
     }
 
     public typeLastName() {
-        browser.pause(2000);
         $(this.lastNameField).waitForVisible();
         $(this.lastNameField).setValue('Last name');
     }
 
     public typeEmail() {
-        browser.pause(2000);
         $(this.emailField).waitForVisible();
         $(this.emailField).setValue(Math.random() + '@gmail.com');
     }
 
     public typePassword() {
-        browser.pause(2000);
         $(this.passwordField).waitForVisible();
         $(this.passwordField).setValue('123321');
     }
 
     public typeConfirmPassword() {
-        browser.pause(2000);
         $(this.confirmPasswordField).waitForVisible();
         $(this.confirmPasswordField).setValue('123321');
     }
 
     public createAccount() {
-        browser.pause(2000);
         $(this.createAccountBtn).waitForVisible();
         $(this.createAccountBtn).click();
     }
